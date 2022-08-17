a9fc42a21d9d5be4d12b5ad9697800cc7c5dfd43
Benjamin Albrecht <benjamin@albrecht.systems>
2022-08-17 08:37:11 -0500

    Finalized some testing - I'm only human

--- GetUsers.ps1
+++ GetUsers.ps1
@@ -1,56 +1,56 @@
 Import-Module WebAdministration; 
 
+# AppPools
 $AppPoolUserInformation = @(
     Get-ChildItem -Path 'IIS:\AppPools\' | 
-        Select-Object -Property Name, @{e={$_.processModel.username};l='Username'}`
+        Select-Object -Property @{e={$_.Name};l='AppPoolName'}, @{e={$_.processModel.username};l='Username'}`
         ,@{e={$_.processModel.password};l='Password'}
 );  
 
 $objAppPoolInfo = $AppPoolUserInformation | ForEach-Object { 
     [PSCustomObject]@{
-        AppPoolName = $_.Name
+        AppPoolName = $_.AppPoolName
         UserName = $_.Username 
         Password = $_.Password
-        Type = 'AppPool'
     }
 }
 
-
+# Services
 $ServiceAccountInfomation = @( 
-    Get-WmiObject -Class win32_service | Select-Object -Property Name, StartName;
+    Get-WmiObject -Class win32_service | Select-Object -Property @{e={$_.Name};l='ServiceName'}, StartName;
 ); 
 
 $objSvcAcctInfo = $ServiceAccountInfomation | ForEach-Object { 
     [PSCustomObject]@{
-        ServiceName = $_.Name
+        ServiceName = $_.ServiceName
         ServiceAccount = $_.StartName
-        Type = 'Service'
     }
 }
 
+# Create an array to hold our objects
 $report = @() 
 
+# AppPools
 foreach ($appPool in $objAppPoolInfo) { 
     $report += [PSCustomObject]@{
     AppPoolName = $appPool.AppPoolName 
     AppPoolUserID = $appPool.AppPoolName
     AppPoolUserPassword = $appPool.Password
-    Type = $appPool.Type
+    ServiceName = '' 
+    ServiceAccount = ''
     }
 }
 
+# Services
 foreach ($service in $objSvcAcctInfo) { 
         $report += [PSCustomObject]@{
-        AppPoolName = ""
-        AppPoolUserID = ""
-        AppPoolUserPassword = ""
+        AppPoolName = '' 
+        AppPoolUserID = '' 
+        AppPoolUserPassword = ''
         ServiceName = $service.ServiceName 
         ServiceAccount =  $service.ServiceAccount
-        Type = $service.Type
-
     }
 }
-
 
 $report
 
